d0abaef518d9d3f9c231768d2266ad0199b4a31a
wym <wym@sorcerer.co.jp>
2024-04-01 12:13:04 +0900

    add host

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ host: "0.0.0.0", port: 8080 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
